5cf946430d32bb9bb1af07e9b5c47d573947ecbd
Jennifer Strater <jlstrater@users.noreply.github.com>
2016-04-24 15:42:06 -0500

    add party parrot wear app generatedf from android studio

---
+++ wear/src/main/java/com/jennstrater/partyparrot/GIFView.java
@@ -0,0 +1,63 @@
+package com.jennstrater.partyparrot;
+
+import java.io.InputStream;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * courtesy of http://www.geeks.gallery/how-to-display-the-animated-gif-image-in-android/
+ */
+public class GIFView extends View {
+    public Movie mMovie;
+    public long movieStart;
+
+    public GIFView(Context context) {
+        super(context);
+        initializeView();
+    }
+
+    public GIFView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeView();
+    }
+
+    public GIFView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeView();
+    }
+
+    private void initializeView() {
+        InputStream is = getContext().getResources().openRawResource(R.raw.parrot);
+        mMovie = Movie.decodeStream(is);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        long now = android.os.SystemClock.uptimeMillis();
+        if (movieStart == 0) {
+            movieStart = now;
+        }
+        if (mMovie != null) {
+            int relTime = (int) ((now - movieStart) % mMovie.duration());
+            mMovie.setTime(relTime);
+            mMovie.draw(canvas, getWidth() - mMovie.width(), getHeight() - mMovie.height());
+            this.invalidate();
+        }
+    }
+
+    private int gifId;
+
+    public void setGIFResource(int resId) {
+        this.gifId = resId;
+        initializeView();
+    }
+
+    public int getGIFResource() {
+        return this.gifId;
+    }
+}
